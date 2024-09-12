94e1f9aeba4510a60e520402e8ad1277b476439c
Victor <victor@Victors-MacBook-Pro-2.local>
2024-09-11 20:54:01 -0400

    Agregar los controladores

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clientes = Cliente::where('nombre', 'like', '%A%')->get();
+        return view('clientes.index', ['clientes' => $clientes]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('clientes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $cliente = new Cliente();
+        $cliente->nombre = $request->nombre;
+        $cliente->telefono = $request->telefono;
+        $cliente->email = $request->email;
+        $cliente->direccion = $request->direccion;
+        $cliente->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $cliente = Cliente::find($id);
+        return view('clientes.show', ['cliente' => $cliente]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $cliente = Cliente::find($id);
+        return view('clientes.edit', ['cliente' => $cliente]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $cliente = Cliente::find($id);
+        $cliente->nombre = $request->nombre;
+        $cliente->telefono = $request->telefono;
+        $cliente->direccion = $request->direccion;
+        $cliente->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $cliente = Cliente::find($id);
+        $cliente->delete();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(ClienteController::class)->group(function(){
+    Route::get('/clientes', 'index');
+    Route::get('/clientes/crear', 'create');
+    Route::post('/clientes/crear', 'store');
+    Route::get('/clientes/ver/{id}', 'show');
+    Route::get('/cliente/editar/{id}', 'edit');
+    Route::put('/clientes/editar/{id}', 'update');
+    Route::delete('/clientes/eliminar/{id}', 'destroy');
+});
+
+
